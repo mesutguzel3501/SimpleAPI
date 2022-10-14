5c02c46bde21d0360ef38659a812d545265451db
mesutguzel3501 <doganguzel@pythian.com>
2022-10-14 10:29:14 -0400

    initial commit

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -2,24 +2,25 @@ using System;
 using Xunit;
 using SimpleAPI.Controllers;
 
-namespace SimpleAPI.Test;
 
-public class UnitTest1
+namespace SimpleAPI.Test
 {
-
-    WeatherForecastController controller = new WeatherForecastController();
-    [Fact]
-
-public void GetReturnsMyName(){
-    var returnValue = controller.Get(1);
-    Assert.Equal(
-        "Mesut Doganguzel",
-        returnValue.Value);
-}
-
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Mesut Doganguzel", returnValue.Value);
+        }
 
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
     }
 }
